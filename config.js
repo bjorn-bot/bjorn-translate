e906c2c92026183404e4535465afa6e6b4ee3410
bharding2 <b.harding@gmail.com>
2019-01-22 13:42:50 -0800

    config file

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "token": process.env.BJORN_TRANSLATE_TOKEN,
+  "prefix": ":t",
+  "owner": ["382724092429598720", "380074303552749568", "402197911376625694", "380041573980700677", "382697158421381122", "159687086318944257"],
+  "playingStatus": "Translating Messages",
+  "tsChannelsEnabled": true
+}
