ae97880e26cec65d57d8388acfee0dfcdd066b7a
bharding2 <b.harding@gmail.com>
2019-02-10 13:37:13 -0800

    init all-sops for bjorn tools

---
+++ bjorn-tools/all-sops.js
@@ -0,0 +1,143 @@
+module.exports = {
+  'kings-landing': {
+    name: 'kings landing',
+    region: 'the-iron-throne',
+    x: 1051, 
+    y: 744,
+    owned: false
+  },
+  'winterfell': {
+    name: 'winterfell',
+    region: 'the-north',
+    x: 748,
+    y: 1905,
+    owned: false
+  },
+  
+  'casterly-rock': {
+    name: 'casterly rock',
+    x: 360,
+    y: 859,
+    width: 14,
+    height: 9
+  },
+  'stone-hedge': {
+    name: 'stone hedge',
+    x: 815,
+    y: 1029,
+    width: 8,
+    height: 6
+  },
+  'bloody-gate': {
+    name: 'bloody gate',
+    x: 1039,
+    y: 1025,
+    width: 8,
+    height: 6
+  },
+  'red-lake': {
+    name: 'red lake',
+    x: 432,
+    y: 651,
+    width: 8,
+    height: 6
+  },
+  'cider-hall': {
+    name: 'cider hall',
+    x: 638,
+    y: 502,
+    width: 8,
+    height: 6
+  },
+  'the-eyrie': {
+    name: 'the eyrie',
+    x: 1039,
+    y: 1081,
+    width: 22,
+    height: 15
+  },
+  'acorn-hall': {
+    name: 'acorn hall',
+    x: 731,
+    y: 991,
+    width: 8,
+    height: 6
+  },
+  'sunspear': {
+    name: 'sunspear',
+    x: 1243,
+    y: 82,
+    width: 22,
+    height: 15
+  },
+  'mistwood': {
+    name: 'mistwood',
+    x: 1154,
+    y: 386,
+    width: 8,
+    height: 6
+  },
+  'pink-maiden': {
+    name: 'pink maiden',
+    x: 665,
+    y: 957,
+    width: 8,
+    height: 6
+  },
+  'river-run': {
+    name: 'river run',
+    x: 690,
+    y: 1038,
+    width: 20,
+    height: 15
+  },
+  'harrenhal': {
+    name: 'harrenhal',
+    x: 881,
+    y: 955,
+    width: 20,
+    height: 20
+  },
+  'wheat-field': {
+    name: 'Wheatfield',
+    x: 843,
+    y: 1229,
+    width: 8,
+    height: 6
+  },
+  'raven-tree-hall': {
+    name: 'Raven Tree Hall',
+    x: 715,
+    y: 1111,
+    width: 8,
+    height: 6
+  },
+  'twins': {
+    name: 'The Twins',
+    x: 719,
+    y: 1285,
+    width: 18,
+    height: 16
+  },
+  'blue-spine': {
+    name: 'Blue Spine',
+    x: 869,
+    y: 850,
+    width: 8,
+    height: 6
+  },
+  'high-garden': {
+    name: 'High Garden',
+    x: 521,
+    y: 438,
+    width: 15,
+    height: 15
+  },
+  'grassy-vale': {
+    name: 'Grassy Vale',
+    x: 880,
+    y: 573,
+    width: 8,
+    height: 6
+  }
+};
